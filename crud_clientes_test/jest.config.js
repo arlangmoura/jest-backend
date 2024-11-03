64613db1df1825a654f8a1e7dd63efa09afa4c57
arlangmoura <45574686+arlangmoura@users.noreply.github.com>
2024-11-03 20:31:05 -0300

    Update jest.config.js

--- crud_clientes_test/jest.config.js
+++ crud_clientes_test/jest.config.js
@@ -5,9 +5,14 @@ module.exports = {
         ['jest-stare', {
             resultDir: 'jest-stare',
             reportTitle: 'Relatório de Testes',
-            additionalResultsProcessors: ['jest-html-reporter'],
-            outputPath: 'jest-stare/report.html', // Especifica onde o relatório HTML será gerado
+            additionalResultsProcessors: ['jest-html-reporter']
+        }],
+        ['jest-html-reporter', {
+            pageTitle: 'Relatório de Testes',
+            outputPath: 'reports/test-report.html',
+            includeFailureMsg: true,
+            includeSuiteFailure: true,
+            customScriptPath: './custom_script.js', // se você tiver um script personalizado
         }]
-    ],
-    testResultsProcessor: 'jest-html-reporter', // Adiciona o processor para gerar o relatório HTML
+    ]
 };
