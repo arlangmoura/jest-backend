8c7052e60eb1b5904a533b3af71e3c84bbce5dec
Arlan Moura <arlan_moura@hotmail.com>
2024-11-03 18:45:07 -0300

    adicionando correções

--- crud_clientes_test/fixtures/jsonUsuarios.js
+++ crud_clientes_test/fixtures/jsonUsuarios.js
@@ -7,23 +7,30 @@ const { faker } = require('@faker-js/faker');
     senha: "1232"
 }*/
 
+const generatePhoneNumber = () => {
+    const ddd = faker.string.numeric(2); // Gera dois dígitos para o DDD
+    const parte1 = faker.string.numeric(4); // Quatro dígitos
+    const parte2 = faker.string.numeric(4); // Quatro dígitos
+    return `+55 (${ddd}) ${parte1}-${parte2}`;
+}
+
 const payloadUsuario = {
     nome: faker.person.fullName(),
-    telefone: faker.phone.number('+55 (##) ####-####'),
+    telefone: generatePhoneNumber(),//telefone: faker.phone.number('+55 (##) ####-####'),
     email: faker.internet.email(),
     senha: faker.internet.password()
 }
 
 const payloadUsuarioEmailFieldNull = {
     nome: faker.person.fullName(),
-    telefone: faker.phone.number('+55 (##) ####-####'),
+    telefone: generatePhoneNumber(),//telefone: faker.phone.number('+55 (##) ####-####'),
     email: null,
     senha: faker.internet.password()
 }
 
 const novoUsuario = {
     nome: faker.person.fullName(),
-    telefone: faker.phone.number('+55 (##) ####-####'),
+    telefone: generatePhoneNumber(),//telefone: faker.phone.number('+55 (##) ####-####'),
     email: faker.internet.email(),
     senha: faker.internet.password()
 }
--- crud_clientes_test/jest.config.js
+++ crud_clientes_test/jest.config.js
@@ -5,7 +5,8 @@ module.exports={
         ['jest-stare', {
             resultDir: 'jest-stare',
             reportTitle: 'Relatório de Testes',
-            additionalResultsProcessors: ['jest-html-reporter']
+            additionalResultsProcessors: ['jest-html-reporter'],
+            outputPath: 'jest-stare/report.html', // caminho para o arquivo do relatório
         }]
     ]
 }
