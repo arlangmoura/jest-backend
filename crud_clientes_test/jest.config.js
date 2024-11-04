52f7a61d5efdde7f10e744a19a65e62640b65faf
Arlan Moura <arlan_moura@hotmail.com>
2024-11-03 21:28:17 -0300

    nova config

--- crud_clientes_test/jest.config.js
+++ crud_clientes_test/jest.config.js
@@ -3,16 +3,18 @@ module.exports = {
     reporters: [
         'default',
         ['jest-stare', {
-            resultDir: 'jest-stare',
-            reportTitle: 'Relatório de Testes',
-            additionalResultsProcessors: ['jest-html-reporter']
+            resultDir: 'jest-stare', // Diretório onde os resultados do Jest Stare serão salvos
+            reportTitle: 'Relatório de Testes', // Título do relatório
+            additionalResultsProcessors: ['jest-html-reporter'] // Processador adicional para relatórios em HTML
         }],
         ['jest-html-reporter', {
-            pageTitle: 'Relatório de Testes',
-            outputPath: 'reports/test-report.html',
-            includeFailureMsg: true,
-            includeSuiteFailure: true,
-            customScriptPath: './custom_script.js', // se você tiver um script personalizado
+            pageTitle: 'Relatório de Testes', // Título da página do relatório
+            outputPath: 'reports/test-report.html', // Caminho onde o relatório HTML será salvo
+            includeFailureMsg: true, // Incluir mensagens de falha
+            includeSuiteFailure: true, // Incluir falhas de suíte
+            customScriptPath: './custom_script.js', // Caminho para um script personalizado (opcional)
         }]
-    ]
+    ],
+    // Adicione esta configuração para gerar relatórios em caso de falha
+    testResultsProcessor: 'jest-stare', // Garante que os resultados sejam processados pelo jest-stare
 };
